b4b89b1b9d73c9b3f51ebb5b1af236d42ff6123c
Tan Le <lenhattan2313@gmail.com>
2022-12-20 10:37:29 +0700

    Intial project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        amazon_blue: {
+          light: "#232F3E",
+          DEFAULT: "#131921",
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/line-clamp")],
+};
